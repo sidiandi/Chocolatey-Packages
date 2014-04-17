4d8d2ca8bb0d0df4ee74848da90d1db095bb1c69
kscelfo <kscelfo@gmail.com>
2014-04-17 13:33:03 -0500

    Adding RoboForm, and incrementing version of Office 365

---
+++ RoboForm/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'RoboForm'
+$installerType = 'EXE'
+$url = 'http://www.roboform.com/dist/RoboForm-Setup.exe'
+$silentArgs = '/silent'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
