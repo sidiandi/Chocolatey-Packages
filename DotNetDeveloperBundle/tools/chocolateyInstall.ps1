a5ed129d548e23abcdfcad3480d63419615bdb80
kscelfo <kscelfo@gmail.com>
2014-04-17 15:12:05 -0500

    Committing DotNetDeveloperBundle

---
+++ DotNetDeveloperBundle/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'DotNetDeveloperBundle'
+$installerType = 'EXE'
+$url = 'http://downloads.red-gate.com/DotNETDeveloperBundle.exe'
+$silentArgs = '/IAgreeToTheEULA'
+$validExitCodes = @(0)
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
