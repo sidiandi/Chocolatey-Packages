1b3ec6973d76c3e2ee39c3dafc93fed2b693e68f
kscelfo <kscelfo@gmail.com>
2014-04-17 15:28:32 -0500

    Initial commit for SQL Toolbelt

---
+++ SqlToolbelt/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'SqlToolbelt'
+$installerType = 'EXE'
+$url = 'http://downloads.red-gate.com/SQLToolbelt.exe'
+$silentArgs = '/IAgreeToTheEula'
+$validExitCodes = @(0)
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
