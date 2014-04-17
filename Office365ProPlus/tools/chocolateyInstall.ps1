dfae7880dce06d3b4addae28de9844d5f2ade312
kscelfo <kscelfo@gmail.com>
2014-04-17 13:06:49 -0500

    committing initial files

---
+++ Office365ProPlus/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿$configFile = (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'configuration.xml')
+$packageName = 'Office365ProPlus'
+$installerType = 'EXE'
+$url = 'http://download.microsoft.com/download/6/2/3/6230F7A2-D8A9-478B-AC5C-57091B632FCF/officedeploymenttool.exe'
+
+$silentArgs = '/extract:$env:temp\office /log:$env:temp\officeInstall.log /quiet /norestart' 
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+Install-ChocolateyInstallPackage "$packageName" "$installerType" "/download $configFile" "$env:temp\office\setup.exe"
+
+Install-ChocolateyInstallPackage "$packageName" "$installerType" "/configure $configFile" "$env:temp\office\setup.exe"
