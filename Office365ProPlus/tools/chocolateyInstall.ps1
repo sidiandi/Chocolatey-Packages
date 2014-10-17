90f1833a9737c6c68dd454a3cf868d206796d0fb
kscelfo <kscelfo>
2014-10-17 10:54:24 -0500

    Correcting issues with Paths that were preventing the package from running. Special thanks to Brandon.

--- Office365ProPlus/tools/chocolateyInstall.ps1
+++ Office365ProPlus/tools/chocolateyInstall.ps1
@@ -3,10 +3,10 @@ $packageName = 'Office365ProPlus'
 $installerType = 'EXE'
 $url = 'http://download.microsoft.com/download/6/2/3/6230F7A2-D8A9-478B-AC5C-57091B632FCF/officedeploymenttool.exe'
 
-$silentArgs = '/extract:$env:temp\office /log:$env:temp\officeInstall.log /quiet /norestart' 
+$silentArgs = "/extract:$env:temp\office /log:$env:temp\officeInstall.log /quiet /norestart"
 $validExitCodes = @(0)
 
+Write-Host "Extracting to $silentArgs"
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
 Install-ChocolateyInstallPackage "$packageName" "$installerType" "/download $configFile" "$env:temp\office\setup.exe"
-
 Install-ChocolateyInstallPackage "$packageName" "$installerType" "/configure $configFile" "$env:temp\office\setup.exe"
